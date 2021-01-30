33f22936aa1e335bb188be3d3abde606abbc03b1
tooPieElf <ndimnezechukwudi@gmail.com>
2021-01-30 20:51:36 +0100

    Initial commit

---
+++ src/main/java/com/webspoons/snippetgen/SnippetgenApplication.java
@@ -0,0 +1,13 @@
+package com.webspoons.snippetgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnippetgenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webspoons/snippetgen/SnippetgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webspoons.snippetgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
